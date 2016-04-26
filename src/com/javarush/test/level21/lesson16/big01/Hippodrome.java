2ae9d9e5dd2d9548c34719df731ccf20522094be
DemianKurilenko <kurilenko.dm@gmail.com>
2016-04-26 06:59:23 +0300

    Initial commit

--- src/com/javarush/test/level21/lesson16/big01/Hippodrome.java
+++ src/com/javarush/test/level21/lesson16/big01/Hippodrome.java
@@ -7,13 +7,27 @@ import java.util.ArrayList;
  */
 public class Hippodrome
 {
+    public static Hippodrome game;
+    public static ArrayList<Horse> horses = new ArrayList<>();
+    Horse bura = new Horse("Bura", 1.5, 3.2);
+    Horse mega = new Horse("Mega", 1.6, 4.2);
+    Horse leka = new Horse("Leka", 1.3, 2.2);
+
     public static void main(String[] args)
     {
+        Hippodrome hipo = new Hippodrome();
+        game = hipo;
+        Horse bura = new Horse("Bura", 1.5, 3.2);
+        Horse mega = new Horse("Mega", 1.6, 4.2);
+        Horse leka = new Horse("Leka", 1.3, 2.2);
+        horses.add(bura);
+        horses.add(mega);
+        horses.add(leka);
     }
-    public static ArrayList<Horse> horses = new ArrayList<>();
+
     public static ArrayList<Horse> getHorses()
     {
-        return horses;//
+        return horses;
     }
 }
 
