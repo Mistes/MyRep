773cf1e39486c6c017c5857ac874ef5ef5012bbf
Mistes <kurilenko-dm@mail.ru>
2016-05-18 07:22:25 +0300

    wednsday

---
+++ src/com/javarush/test/level14/lesson08/home05/Mouse.java
@@ -0,0 +1,13 @@
+package com.javarush.test.level14.lesson08.home05;
+
+/**
+ * Created by Mistes on 18.05.2016.
+ */
+public class Mouse implements CompItem{
+    @Override
+    public String getName()
+    {
+        return "Mouse";
+    }
+
+}
